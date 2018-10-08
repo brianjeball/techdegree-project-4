2633c85cfdebdaebebc7dbc908912b3941a83d5a
Brian Ball <brianbll9@gmail.com>
2018-10-08 09:32:59 -0400

    update

--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,16 @@
 class Game {
-  constructor(missed, phrases) {
-    this.missed = missed;
-    this.phrases = phrases[hello, golden
+  constructor() {
+    this.missed = 0;
+    this.phrases = [
+      'hello',
+      'golden',
+      'panthers'
     ];
   }
 
   getRandomPhrase() {
-
+    var randomPhrase = phrases[Math.floor(Math.random()*phrases.length)]; /* Selects random quote from the quotes array.*/
+    return (RandomQuote); /* Returns the randomly selected quote object. */
   }
 
   handleInteraction() {
@@ -31,5 +35,5 @@ class Game {
   }
 
   startGame() {
-    
+
   }
